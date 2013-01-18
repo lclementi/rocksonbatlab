00c48938005fd47ea7f8d3e37c18d33f36e56047
Luca Clementi <luca.clementi@gmail.com>
2013-01-17 21:19:32 -0600

    Fixed reload env problem, save some space

--- rar-test.sh
+++ rar-test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# remote machine for staging output of compilation
+RemoteMachine=calit2-119-136.ucsd.edu
+RemotePath=upload
+RemoteUser=upload
+
+# used to figure out input file name for source code
 Date=`date +%F`
 FileName=rocks-source-$Date.tar.gz
 
@@ -9,6 +15,9 @@ wget ftp://ftp.rocksclusters.org/pub/rocks/src/$FileName.md5
 md5sum -c $FileName.md5 || (echo Error file checksum; exit -1;)
 
 tar -xvzf $FileName || (echo Error untarring file; exit -1;)
+
+#let free up some space
+rm $FileName
 
 cd rocks
 
@@ -31,13 +40,20 @@ popd
 echo ----  Running builder script  ----
 . /etc/profile
 . /etc/profile.d/rocks-binaries.sh
-sudo -i ./builder.sh < /dev/null 
+sudo bash -l builder.sh < /dev/null 
 
 echo ----  End builder script      ----
 
 cd ..
 ## ---- fetch all the output files
 tar -czvf results.tar.gz /tmp/*.out
+
+# stage them to remote machine
+RemoteDest=$RemoteUser@$RemoteMachine:~$RemotePath/$Date
+
+chmod 600 id_rsa
+#TODO finish staging out results
+echo scp -i id_rsa results.tar.gz $RemoteDest
 
 exit 0
 
